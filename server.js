543b1926cd78d781feddcf6b128b10d4c9f97486
BuildTools <unconfigured@null.spigotmc.org>
2018-05-21 22:25:28 +0200

    finally fixed deploy :)

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-// server.js
 const express = require('express');
+const http = require('http')
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static(path.join(__dirname, 'dist/Shopping')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/Shopping/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
